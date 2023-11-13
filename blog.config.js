305c6468c9102dc90efffd9ce8d802ab942aca25
elfishden256 <70300901+elfishden256@users.noreply.github.com>
2023-11-13 11:23:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: '梦的记录',
+  author: 'el',
+  email: 'wjx0226@foxmail.com',
+  link: 'https://blog.mengdejilu.net',
+  description: 'In this plausible world',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
